ba665c5355024d9bbe272ba4a6a4cddea5ab11ec
o-niki-ri <nikateran.research@gmail.com>
2026-01-27 10:14:49 +1100

    Open external links in new tab

---
+++ docs/javascripts/external-links.js
@@ -0,0 +1,9 @@
+document$.subscribe(function() {
+  var links = document.querySelectorAll('a[href^="http"]');
+  links.forEach(function(link) {
+    if (!link.href.includes(window.location.hostname)) {
+      link.setAttribute('target', '_blank');
+      link.setAttribute('rel', 'noopener noreferrer');
+    }
+  });
+});
