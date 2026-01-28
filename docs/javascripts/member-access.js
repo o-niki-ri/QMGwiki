72f1f7c336a84335a1d1fd68f6218ff91ae3b1de
o-niki-ri <nikateran.research@gmail.com>
2026-01-28 19:31:48 +1100

    Update member access section

--- docs/javascripts/member-access.js
+++ docs/javascripts/member-access.js
@@ -1,9 +1,6 @@
 document$.subscribe(function() {
   document.querySelectorAll('a[href*="member-access"]').forEach(function(link) {
-    link.addEventListener("click", function(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      window.location.href = link.href;
-    }, true);
+    link.setAttribute("target", "_blank");
+    link.setAttribute("rel", "noopener");
   });
 });
